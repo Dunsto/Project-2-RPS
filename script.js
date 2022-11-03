b3cfbcba2024fad2c201dd048736a4a8dbcc087b
Alex <Bohnson@gmail.com>
2022-11-02 23:38:39 -0700

    RPS Beginnings

--- script.js
+++ script.js
@@ -8,4 +8,44 @@ function main() {
     }
 }
 
-main();
+let playerSelection;
+
+function beginGame() {
+    document.querySelector('#game-start')
+    .addEventListener("click", () => {
+        document.querySelector("#game-start").style.display = "none";
+        document.querySelector("#shoot").style.display = "block";
+        listenForChoice();
+    })
+}
+
+function listenForChoice(){
+    const choices = document.querySelectorAll('.choice')
+    choices.forEach((choice, i, arr) => {
+        choice.addEventListener('click', (e) => {
+            resetChoiceColor();
+            playerSelection = arr[i].id;
+            console.log(`${playerSelection}`);
+            e.target.style.color = "#DAA520";
+            e.target.style.opacity = "1";
+            console.log(`${e.target}`);
+
+
+        })
+    });
+}
+
+function resetChoiceColor(){
+    const choices = document.querySelectorAll('.choice')
+    choices.forEach(choice => {
+        for (const attr of choice.attributes) {
+            console.log(attr)
+        }
+
+       // console.log(choice.firstChild.nodeName)
+      //  choice.firstChild.style.color =  'rgb(59, 62, 62)';
+    })
+
+}
+
+beginGame();
